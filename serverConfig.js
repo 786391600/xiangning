d0e71782ddd4380839dd69678b76997837aac6b5
wangtao <l>
2019-11-13 19:13:09 +0800

    new

--- serverConfig.js
+++ serverConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //serverUrl: 'https://www.beijixiong.club/car'
-  serverUrl: 'http://localhost:18080/action'
+  serverUrl: 'https://www.beijixiong.club/car'
+  //serverUrl: 'http://localhost:18081/action'
 }
